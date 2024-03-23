a42aea09c9bf332c849fa68438a6889a792a9801
Pauline Hannah <90203913+Paulinehan13@users.noreply.github.com>
2024-03-23 22:36:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+<!-- Image and text -->
+<nav class="navbar navbar-expand-lg" style="background-color: rgb(255, 255, 255, 0);">
+  <a class="navbar-brand" href="#">
+    <img src="aurct-logo.png" width="55px" height="55px" style="margin-top: 15px; margin-left: 20px;"
+      class="d-inline-block align-top" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div style="width:700px"></div>
+  <div class="collapse navbar-collapse" id="navbarNav" style="justify-content: space-between;"
+    style="background-color: transparent;">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" style="margin-right:50px; font-size: 17px; color:gray ;" href="#">Home <span
+            class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="margin-right:50px; font-size: 17px; " href="#">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="margin-right:50px; font-size: 17px;" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="margin-right:50px; font-size: 17px;" href="#">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
